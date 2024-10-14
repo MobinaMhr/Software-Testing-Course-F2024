33e6e8d6f9c7b057a4f39e6e2463265664647139
Mohammadreza <mrmh13801225@gmail.com>
2024-10-14 17:45:02 +0330

    Table test adding reservation method implemented

---
+++ Mizdooni/src/test/java/mizdooni/model/TableTest.java
@@ -0,0 +1,38 @@
+package mizdooni.model;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+public class TableTest {
+    private Restaurant restaurant;
+    private Table table;
+    private Reservation reservation1;
+    private Reservation reservation2;
+    private User manager;
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    public void setUp() {
+        Address address = new Address("Iran", "Tehran", "Kargar");
+        manager = new User("Akbar Akbari", "password", "AkbarAkbari@example.com", address,
+                User.Role.manager);
+        user1 = new User("mmd", "password", "mmd@example.com", address, User.Role.client);
+        user2 = new User("mobina", "password", "mobina@example.com", address, User.Role.client);
+        restaurant = new Restaurant("Baradaran Akbari bejoz Davood", manager, "Kababi", LocalTime.now(),
+                LocalTime.now().plusHours(8), "100% goosfandi", address, ":|");
+        table = new Table(1, restaurant.getId(), 4);
+        reservation1 = new Reservation(user1, restaurant, table, LocalDateTime.now());
+        reservation2 = new Reservation(user2, restaurant, table, LocalDateTime.now().plusHours(1));
+    }
+
+    @Test
+    public void testAddReservation(){
+        table.addReservation(reservation1);
+        assertEquals(1, table.getReservations().size());
+        assertEquals(reservation1, table.getReservations().getFirst());
+    }
+}
--- Mizdooni/src/test/java/mizdooni/model/UserTest.java
+++ Mizdooni/src/test/java/mizdooni/model/UserTest.java
@@ -2,9 +2,7 @@ package mizdooni.model;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
     private Address address;
