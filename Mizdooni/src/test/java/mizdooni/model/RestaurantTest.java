7821df1c7091173f62212cd28d26e057a564582f
Mohammadreza <mrmh13801225@gmail.com>
2024-10-21 23:06:48 +0330

    test check average rating

--- Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
+++ Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
@@ -150,4 +150,22 @@ public class RestaurantTest {
 
     }
 
+    @Test
+    @DisplayName("check averagerating")
+    public void testCheckAverageRating(){
+        Rating rating1 = new Rating(){{food = 4; ambiance = 4.2; service = 3.7; overall= 4.1;}};
+        Rating rating2 = new Rating(){{food = 4; ambiance = 4.1; service = 4.1; overall= 4.5;}};
+        Review review1 = new Review(user1, rating1, "This restaurant was perfect.", LocalDateTime.now());
+        Review review2 = new Review(user2, rating2, "This restaurant was not perfect.", LocalDateTime.now());
+
+        restaurant.addReview(review1);
+        restaurant.addReview(review2);
+
+        Rating averageRating = restaurant.getAverageRating();
+        assertEquals(4, averageRating.food);
+        assertEquals(4.15, averageRating.ambiance);
+        assertEquals(3.9, averageRating.service);
+        assertEquals(4.3, averageRating.overall);
+    }
+
 }
