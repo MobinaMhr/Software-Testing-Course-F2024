17cb40d443cb59fca328604f1d861e1fe7d56d4f
Mohammadreza <mrmh13801225@gmail.com>
2024-10-28 02:18:25 +0330

    implement test checking correct logged in user without implementing response getters

--- Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
@@ -1,4 +1,5 @@
 package mizdooni.controllers;
+import mizdooni.model.User;
 import mizdooni.service.*;
 import mizdooni.database.*;
 import mizdooni.response.*;
@@ -9,8 +10,14 @@ import org.springframework.http.HttpStatus;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationControllerTest {
+
+    @Mock
+    private User user1;
+    @Mock
+    private User user2;
     @Mock
     private UserService userService;
 
@@ -20,5 +27,17 @@ public class AuthenticationControllerTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testUser_LoggedIn() {//TODO: should implement getters for response ?
+
+        when(userService.getCurrentUser()).thenReturn(user1);
+
+        Response response = authenticationController.user();
+        assertEquals("current user", response.getMessage());
+        assertEquals(user1, response.getData());
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(true, response.isSuccess());
     }
 }
