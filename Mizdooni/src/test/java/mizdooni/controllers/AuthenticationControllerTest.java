08f7f08e2958fc01811b0ec8caf6a6a4b4ed24a5
Mohammadreza <mrmh13801225@gmail.com>
2024-10-28 01:40:47 +0330

    fix dependencies for mockito initial HW2 commit

---
+++ Mizdooni/src/test/java/mizdooni/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,14 @@
+package mizdooni.controllers;
+import mizdooni.service.*;
+import mizdooni.database.*;
+import mizdooni.response.*;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthenticationControllerTest {
+}
