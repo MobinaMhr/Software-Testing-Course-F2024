8beeb46be4f006f281410bd71c2827b64734afca
Mohammadreza <mrmh13801225@gmail.com>
2024-10-28 22:50:13 +0330

    setup

---
+++ Mizdooni/src/test/java/mizdooni/controllers/ReviewControllerTest.java
@@ -0,0 +1,26 @@
+package mizdooni.controllers;
+import mizdooni.model.*;
+import mizdooni.response.*;
+import mizdooni.service.*;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+import org.springframework.http.HttpStatus;
+import java.util.*;
+
+
+public class ReviewControllerTest {
+    @Mock
+    private RestaurantService restaurantService;
+
+    @Mock
+    private ReviewService reviewService;
+
+    @InjectMocks
+    private ReviewController reviewController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+}
