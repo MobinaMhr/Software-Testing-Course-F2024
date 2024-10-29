3acfe91feb802988a828f626a1d3e820308e10b0
Mohammadreza <mrmh13801225@gmail.com>
2024-10-29 09:51:29 +0330

    add testFindAvailableTableFailToFindRestaurant

--- Mizdooni/src/test/java/mizdooni/controllers/ReservationControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/ReservationControllerTest.java
@@ -47,5 +47,17 @@ public class ReservationControllerTest {
         assertEquals(ControllerUtils.PARAMS_BAD_TYPE, exception.getMessage());
     }
 
+    @Test
+    public void testFindAvailableTableFailToFindRestaurant(){
+        String validDate = "2024-11-01";
+//        List<Table> emptyTables = new ArrayList<>();
+//        when(restaurant.getTables()).thenReturn(emptyTables);
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(null);
+        ResponseException exception = assertThrows(ResponseException.class,
+                () -> reservationController.getReservations(restaurant.getId(), 1, validDate));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+        assertEquals("restaurant not found", exception.getMessage());
+    }
+
 
 }
