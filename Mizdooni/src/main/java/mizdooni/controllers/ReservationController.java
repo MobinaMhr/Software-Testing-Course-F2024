0fd4f2486ef1271567c02eb7bd3298e19bee3eb7
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-11-01 19:53:29 +0330

    Fix a bug in getReservations method of ReservationController

--- Mizdooni/src/main/java/mizdooni/controllers/ReservationController.java
+++ Mizdooni/src/main/java/mizdooni/controllers/ReservationController.java
@@ -32,8 +32,16 @@ public class ReservationController {
                                     @RequestParam(required = false) String date) {
         ControllerUtils.checkRestaurant(restaurantId, restaurantService);
         LocalDate localDate = null;
+//        if (date != null) {
+//            try {
+//                localDate = LocalDate.parse(date, DATE_FORMATTER);
+//            } catch (Exception ex) {
+//                throw new ResponseException(HttpStatus.BAD_REQUEST, PARAMS_BAD_TYPE);
+//            }
+//        }
         try {
-            localDate = LocalDate.parse(date, DATE_FORMATTER);
+            if (date != null)
+                localDate = LocalDate.parse(date, DATE_FORMATTER);
         } catch (Exception ex) {
             throw new ResponseException(HttpStatus.BAD_REQUEST, PARAMS_BAD_TYPE);
         }
