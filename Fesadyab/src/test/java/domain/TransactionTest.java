dc350161c8f477830d333c91772bf9450b856902
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-11-12 10:31:57 +0330

    Add tests for Transaction Class

---
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransactionTest {
+    Transaction transaction;
+    @BeforeEach
+    void setUp() {
+        transaction = new Transaction(){{transactionId=1; accountId=10; amount=1500; isDebit=false;}};
+    }
+
+    @Test
+    void testTxnEqualsWithDummyObject() {
+        Object dummy = new Object();
+        assertEquals(false, transaction.equals(dummy));
+    }
+
+    @Test
+    void testTxnEqualsWithSameTxnObject() {
+        assertEquals(true, transaction.equals(transaction));
+    }
+
+    @Test
+    void testTxnEqualsWithDifferentTxnObject() {
+        Transaction txn = new Transaction(){{transactionId=2; accountId=10; amount=1500; isDebit=false;}};
+        assertEquals(false, transaction.equals(txn));
+    }
+
+}
